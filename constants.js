d12312d1e2efc2314a26dc4b1870a69cb9315b4c
justbytes <100870863+justbytes@users.noreply.github.com>
2025-04-25 09:06:31 -0700

    adding constants file

---
+++ constants.js
@@ -0,0 +1,6 @@
+export const PUMPFUN_PROGRAM_ID = '6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P';
+export const PUMPFUN_GLOBAL = '';
+export const PUMPFUN_FEE_RECIPIENT = '';
+export const PUMPFUN_BONDING_CURVE = '';
+export const PUMPFUN_ASSOCIATED_BONDING_CURVE = '';
+export const PUMPFUN_ASSOCIATED_USER = '';
