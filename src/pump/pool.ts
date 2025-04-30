ab2b5faba92118b0b0f44edc1497cc99e38e9fc3
justbytes <100870863+justbytes@users.noreply.github.com>
2025-04-30 15:52:18 -0700

    Now able to get pumpswap pool pdas and pool authority pdas along with global config pda

--- src/pump/pool.ts
+++ src/pump/pool.ts
@@ -1,6 +1,41 @@
-// Get pool data
+import BN from 'bn.js';
+import { address, Address, getAddressEncoder, getProgramDerivedAddress } from 'gill';
+import { PUMPSWAP_PROGRAM_ID } from './constants';
 
-import { Address } from 'gill';
+export const CANONICAL_POOL_INDEX = 0;
+
+export async function getPumpPoolAuthorityPda(mint: Address) {
+  const [pumpPoolAuthorityPda, pumpPoolAuthorityPdaBump] = await getProgramDerivedAddress({
+    seeds: ['pool-authority', getAddressEncoder().encode(mint)],
+    programAddress: address(PUMPSWAP_PROGRAM_ID),
+  });
+
+  return pumpPoolAuthorityPda;
+}
+
+export const getPoolPda = async (owner: Address, baseMint: Address, quoteMint: Address) => {
+  const [poolPda, _poolPdaBump] = await getProgramDerivedAddress({
+    seeds: [
+      'pool',
+      new BN(CANONICAL_POOL_INDEX).toArrayLike(Buffer, 'le', 2),
+      getAddressEncoder().encode(owner),
+      getAddressEncoder().encode(baseMint),
+      getAddressEncoder().encode(quoteMint),
+    ],
+    programAddress: address(PUMPSWAP_PROGRAM_ID),
+  });
+
+  return poolPda;
+};
+
+export const getPoolData = async (mint: Address, connection: any) => {
+  const [pool, _poolBump] = await getProgramDerivedAddress({
+    seeds: ['pool', getAddressEncoder().encode(mint)],
+    programAddress: address(PUMPSWAP_PROGRAM_ID),
+  });
+
+  return pool;
+};
 
 export const estimatePumpswapMinTokensOut = (
   mint: Address,
