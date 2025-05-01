e2886cc1b829fbfde41d16e1c565db4c3d123834
justbytes <100870863+justbytes@users.noreply.github.com>
2025-04-30 18:49:53 -0700

    Now can get decoded data from pumpswap pool

--- src/pump/pool.ts
+++ src/pump/pool.ts
@@ -1,19 +1,34 @@
-import BN from 'bn.js';
+import * as borsh from '@coral-xyz/borsh';
 import { address, Address, getAddressEncoder, getProgramDerivedAddress } from 'gill';
-import { PUMPSWAP_PROGRAM_ID } from './constants';
+import { PUMPFUN_PROGRAM_ID, PUMPSWAP_PROGRAM_ID } from './constants';
+import BN from 'bn.js';
 
 export const CANONICAL_POOL_INDEX = 0;
+
+// Structure of the Pool
+export const poolDataSchema = borsh.struct([
+  borsh.array(borsh.u8(), 8, 'discriminator'),
+  borsh.u8('pool_bump'),
+  borsh.u16('index'),
+  borsh.publicKey('creator'),
+  borsh.publicKey('base_mint'),
+  borsh.publicKey('quote_mint'),
+  borsh.publicKey('lp_mint'),
+  borsh.publicKey('popool_base_token_accounto'),
+  borsh.publicKey('pool_quote_token_account'),
+  borsh.u64('lp_supply'),
+]);
 
 export async function getPumpPoolAuthorityPda(mint: Address) {
   const [pumpPoolAuthorityPda, pumpPoolAuthorityPdaBump] = await getProgramDerivedAddress({
     seeds: ['pool-authority', getAddressEncoder().encode(mint)],
-    programAddress: address(PUMPSWAP_PROGRAM_ID),
+    programAddress: address(PUMPFUN_PROGRAM_ID),
   });
 
   return pumpPoolAuthorityPda;
 }
 
-export const getPoolPda = async (owner: Address, baseMint: Address, quoteMint: Address) => {
+export const getPumpPoolPda = async (owner: Address, baseMint: Address, quoteMint: Address) => {
   const [poolPda, _poolPdaBump] = await getProgramDerivedAddress({
     seeds: [
       'pool',
@@ -28,13 +43,22 @@ export const getPoolPda = async (owner: Address, baseMint: Address, quoteMint: A
   return poolPda;
 };
 
-export const getPoolData = async (mint: Address, connection: any) => {
-  const [pool, _poolBump] = await getProgramDerivedAddress({
-    seeds: ['pool', getAddressEncoder().encode(mint)],
-    programAddress: address(PUMPSWAP_PROGRAM_ID),
-  });
+export const getPumpPoolData = async (baseMint: Address, quoteMint: Address, connection: any) => {
+  // Get pool authority/ owner
+  const pumpPoolAuthorityPda = await getPumpPoolAuthorityPda(baseMint);
 
-  return pool;
+  // Get pool Pda
+  const pumpPoolPda = await getPumpPoolPda(pumpPoolAuthorityPda, baseMint, quoteMint);
+
+  // Get pumpPool account info
+  const pumpPoolAccountInfo = await connection.rpc
+    .getAccountInfo(pumpPoolPda, { encoding: 'base64' })
+    .send();
+
+  const base64Data = pumpPoolAccountInfo.value.data[0];
+  const dataBuffer = Buffer.from(base64Data, 'base64');
+
+  return poolDataSchema.decode(dataBuffer);
 };
 
 export const estimatePumpswapMinTokensOut = (
